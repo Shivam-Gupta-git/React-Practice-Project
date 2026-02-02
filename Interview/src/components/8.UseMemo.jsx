b3d8a8046bec886c83499aa59a082d7f2a129e0e
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-02-03 01:04:20 +0530

    UseMemo

--- Interview/src/components/8.UseMemo.jsx
+++ Interview/src/components/8.UseMemo.jsx
@@ -11,6 +11,8 @@ function UseMemoExample() {
     return counterOne % 2 === 0;
   }, [counterOne]);
 
+  
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white shadow-lg rounded-xl p-6 w-80 text-center space-y-4">
